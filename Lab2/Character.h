63905365cc6563d6f0b93b1f822b881066777c90
Ethan Dolman <EthanAndrew.Dolman@georgebrown.ca>
2021-02-04 11:59:05 -0500

    quick fix

--- Lab2/Character.h
+++ Lab2/Character.h
@@ -15,7 +15,7 @@ public:
 	void SetHealth(float health) { this->health = health; };
 	void SetWeapon(string name, string description, double damage, string special_ability1, string special_ability2)
 	{ this->weapon = new Weapon(name, description, damage, special_ability1, special_ability2); }
-	virtual void DisplayInfo(){};
+	virtual void DisplayInfo() = 0;
 	virtual ~Character(){};
 
 	//Weapon weapon;
